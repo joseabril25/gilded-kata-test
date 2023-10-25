eab6cb2006582422e655c89c0ff10adcba764638
Jose Abril Jr <jose.abril4@gmail.com>
2023-10-25 18:49:33 +0800

    separated Item types and created update.ts file for update functions

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,14 +1,5 @@
-export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+import { Item } from "./item";
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
 
 export class GildedRose {
   items: Array<Item>;
